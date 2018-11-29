91a1da966f462b1aee9316978534e4a07fc3c0e3
Tsutomu Yamaguchi <t.yamaguchi.air@outlook.jp>
2018-11-29 22:50:52 +0900

    Initial Commit

---
+++ openGLdemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openGLdemo
+//
+//  Created by 山口勉 on 2018/11/29.
+//  Copyright © 2018年 山口勉. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
